6c1a232d0160107f0d57ae0f2047d6a882b358d8
danielbrunvoll <danielnilsenbrunvoll@gmail.com>
2026-09-02 09:13:55 +0200

    Claude things

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import RootLayout from './views/layouts/RootLayout.jsx'
 import HomePage from './views/pages/HomePage.jsx'
 import ProjectsPage from './views/pages/ProjectsPage.jsx'
@@ -10,15 +11,18 @@ import './App.css'
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<RootLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="projects" element={<ProjectsPage />} />
-        <Route path="projects/:slug" element={<ProjectDetailPage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="contact" element={<ContactPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="projects" element={<ProjectsPage />} />
+          <Route path="projects/:slug" element={<ProjectDetailPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      <Analytics />
+    </>
   )
 }
